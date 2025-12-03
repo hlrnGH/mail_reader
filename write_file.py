6b40c0b96a438f87a88256f3c14647116562a70d
hlrnGH <68645772+hlrnGH@users.noreply.github.com>
2025-12-03 17:02:22 +0100

    added initial config for write_file

---
+++ write_file.py
@@ -0,0 +1,14 @@
+from classifier import classify_urgency
+from read_mails import retrieve_message_list
+
+from dotenv import load_dotenv
+from groq import Groq
+
+import os
+
+# get api key from .env
+load_dotenv()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+
+#def write_classified_messages_in_sheet(classified_messages):
